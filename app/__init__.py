4859f81d5dfcc76a7de43b8d13abb22207c778a1
Edson Silva <edsonoguera@outlook.com>
2020-11-24 10:55:53 -0300

    main

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from app.views.index_view import bp as index_bp
+from app.views.receita_view import bp as receitas_bp
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(index_bp)
+    app.register_blueprint(receitas_bp)
+
+    return app
